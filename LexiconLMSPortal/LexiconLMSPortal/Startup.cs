405995b5f35fa0c63737b8b68c9798eb327cb962
Mattias Rehnvall <Rehnvall15@gmail.com>
2017-07-07 10:12:57 +0200

    Init

---
+++ LexiconLMSPortal/LexiconLMSPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LexiconLMSPortal.Startup))]
+namespace LexiconLMSPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
