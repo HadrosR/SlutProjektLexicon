28f7768f76cf719ceabcceb31c09f10ba58b3f75
Mattias Rehnvall <Rehnvall15@gmail.com>
2017-07-10 13:10:24 +0200

    Added Teacher index View

--- LexiconLMSPortal/LexiconLMSPortal/Controllers/TeacherController.cs
+++ LexiconLMSPortal/LexiconLMSPortal/Controllers/TeacherController.cs
@@ -3,15 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using LexiconLMSPortal.Models.Classes;
+using LexiconLMSPortal.Models.Identity;
+using LexiconLMSPortal.Models.ViewModels;
 
 namespace LexiconLMSPortal.Controllers
 {
     public class TeacherController : Controller
     {
+        ApplicationDbContext context = new ApplicationDbContext();
+
         // GET: Teacher
         public ActionResult Index()
         {
-            return View();
+            
+            var courses = context.Courses;
+            //skapar en ny lista
+            List<CourseViewModel> aktivCourses = new List<CourseViewModel>();
+            //fyller den nya listan med alla curser från CourseViewModel
+            foreach (var c in courses)
+            {
+                aktivCourses.Add(new CourseViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Description = c.Description,
+                    StartDate = c.StartDate,
+                    EndDate = c.EndDate
+
+                });
+            }
+            return View(aktivCourses);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
