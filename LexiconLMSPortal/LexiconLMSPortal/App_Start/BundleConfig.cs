68949bf32e8b242ff35b7f4c6431cb6911f5055a
Henrik Forslin <zyrrewow@hotmail.com>
2017-07-11 14:20:40 +0200

    Added script file, bundled it. Added accordion to course view. Added more seeds. Re-create your local databases.

--- LexiconLMSPortal/LexiconLMSPortal/App_Start/BundleConfig.cs
+++ LexiconLMSPortal/LexiconLMSPortal/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace LexiconLMSPortal
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/LexiconLMS").Include(
+                "~/Scripts/Scripts.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
---
+++ LexiconLMSPortal/LexiconLMSPortal/Scripts/Scripts.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+
+    // Collapse Module Accordion
+    $("body").on("click", ".module-accordion-heading", function (e) {
+        var collapsed = $(this).nextUntil(".module-accordion-link");
+        collapsed.collapse({
+            'parent': "#module-accordion",
+            "toggle": false
+        });
+        collapsed.collapse('toggle');
+
+    });
+
+});
