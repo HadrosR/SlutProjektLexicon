154abf9a25f62c03b0e62fb9427108f8b040f50b
Thomas Myrsten <longusbenus@hotmail.com>
2017-07-17 09:48:03 +0200

    Added support for student list partial view.

--- LexiconLMSPortal/LexiconLMSPortal/Controllers/StudentController.cs
+++ LexiconLMSPortal/LexiconLMSPortal/Controllers/StudentController.cs
@@ -8,12 +8,32 @@ using System.Web;
 using System.Web.Mvc;
 using LexiconLMSPortal.Models.Classes;
 using LexiconLMSPortal.Models.Identity;
+using LexiconLMSPortal.Models.ViewModels;
 
 namespace LexiconLMSPortal.Controllers
 {
     public class StudentController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        
+        public ActionResult _StudentListPartial(int id)
+        {
+            List<_StudentListPartial> sl = new List<_StudentListPartial>();
+            var students = db.Courses.FirstOrDefault(t => t.Id == id).Students;
+
+            foreach (var s in students)
+            {
+                sl.Add(new _StudentListPartial
+                {
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    EMail = s.Email,
+                    Id = s.Id
+
+                });
+            }
+            return View(sl);
+        }
 
         // GET: Student
         public ActionResult Index()
